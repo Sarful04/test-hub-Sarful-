19f06f190673290a3dfca46d7c0ccc951a491338
Akash Verma <ankitv.vr2306@gmail.com>
2023-03-01 15:40:03 +0530

    created student login page

---
+++ lib/featured/Student/S_login.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class S_login extends StatefulWidget {
+  const S_login({super.key});
+
+  @override
+  State<S_login> createState() => _S_loginState();
+}
+
+class _S_loginState extends State<S_login> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
