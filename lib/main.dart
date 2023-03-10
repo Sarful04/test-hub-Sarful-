ae7d9c80e3958633ac3f0e35a885d8d8993549f1
Sunilkushwah7289 <knowledgenetwork18@gmail.com>
2023-03-10 11:05:57 +0530

    modified in teacher screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:test_hub/featured/Student/S_login.dart';
+import 'package:test_hub/featured/Teacher/T_home.dart';
+import 'package:test_hub/featured/Teacher/groups.dart';
+import 'package:test_hub/featured/Teacher/groups_option.dart';
 import 'package:test_hub/featured/Teacher/register.dart';
 import 'package:test_hub/screens/splash_screen.dart';
 import 'package:test_hub/services/authentication.dart';
@@ -14,9 +17,12 @@ void main() async {
   );
   runApp(MaterialApp(
     routes: {
-      "/": (context) => SplashScreen(),
-      "/register": (context) => RegisterPage(),
-      "/S_login": (context) => LoginPage(),
+      "/": (context) => GroupsPage(),
+      "/T_home": (context) => THomePage(),
+      "/groups_option": (context) => Groups_Option(),
+      // "/": (context) => SplashScreen(),
+      // "/register": (context) => RegisterPage(),
+      // "/S_login": (context) => LoginPage(),
     },
     debugShowCheckedModeBanner: false,
     // home: AuthService().handleAuthState(),
